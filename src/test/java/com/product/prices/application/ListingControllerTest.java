adff94e0a0e0d61b8db58777995368f79a8a6278
Uriel <urisepe@gmail.com>
2024-06-16 17:53:07 +0200

    chore: add more test cases to controller

--- src/test/java/com/product/prices/application/ListingControllerTest.java
+++ src/test/java/com/product/prices/application/ListingControllerTest.java
@@ -2,7 +2,6 @@ package com.product.prices.application;
 
 import com.product.prices.PricesListingsApplication;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -26,39 +25,90 @@ public class ListingControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    private static final String TEST_BRAND = "1";
+    private static final String TEST_PRODUCT = "35455";
+
+    private static final ExpectedOutput PRICE_LIST_ONE = new ExpectedOutput(
+            TEST_PRODUCT,
+            TEST_BRAND,
+            "1",
+            "2020-06-14T00:00:00",
+            "2020-12-31T23:59:59",
+            "EUR",
+            35.5
+    );
+
+    private static final ExpectedOutput PRICE_LIST_TWO = new ExpectedOutput(
+            TEST_PRODUCT,
+            TEST_BRAND,
+            "2",
+            "2020-06-14T15:00:00",
+            "2020-06-14T18:30:00",
+            "EUR",
+            25.45
+    );
+
+    private static final ExpectedOutput PRICE_LIST_THREE = new ExpectedOutput(
+            TEST_PRODUCT,
+            TEST_BRAND,
+            "3",
+            "2020-06-15T00:00:00",
+            "2020-06-15T11:00:00",
+            "EUR",
+            30.5
+    );
+
+    private static final ExpectedOutput PRICE_LIST_FOUR = new ExpectedOutput(
+            TEST_PRODUCT,
+            TEST_BRAND,
+            "4",
+            "2020-06-15T16:00:00",
+            "2020-12-31T23:59:59",
+            "EUR",
+            38.95
+    );
+
     public static Stream<Arguments> provideInputForPriceCheck() {
         return Stream.of(
                 Arguments.of(
                         new Input(
-                                "1",
-                                "35455",
+                                TEST_BRAND,
+                                TEST_PRODUCT,
                                 LocalDateTime.of(2020, 6, 14, 10, 0).toString()
                         ),
-                        new ExpectedOutput(
-                                "35455",
-                                "1",
-                                "1",
-                                "2020-06-14T00:00:00",
-                                "2020-12-31T23:59:59",
-                                "EUR",
-                                35.5
-                        )
+                        PRICE_LIST_ONE
                 ),
                 Arguments.of(
                         new Input(
-                                "1",
-                                "35455",
+                                TEST_BRAND,
+                                TEST_PRODUCT,
                                 LocalDateTime.of(2020, 6, 14, 16, 0).toString()
                         ),
-                        new ExpectedOutput(
-                                "35455",
-                                "1",
-                                "2",
-                                "2020-06-14T15:00:00",
-                                "2020-06-14T18:30:00",
-                                "EUR",
-                                25.45
-                        )
+                        PRICE_LIST_TWO
+                ),
+                Arguments.of(
+                        new Input(
+                                TEST_BRAND,
+                                TEST_PRODUCT,
+                                LocalDateTime.of(2020, 6, 14, 21, 0).toString()
+                        ),
+                        PRICE_LIST_ONE
+                ),
+                Arguments.of(
+                        new Input(
+                                TEST_BRAND,
+                                TEST_PRODUCT,
+                                LocalDateTime.of(2020, 6, 15, 10, 0).toString()
+                        ),
+                        PRICE_LIST_THREE
+                ),
+                Arguments.of(
+                        new Input(
+                                TEST_BRAND,
+                                TEST_PRODUCT,
+                                LocalDateTime.of(2020, 6, 16, 21, 0).toString()
+                        ),
+                        PRICE_LIST_FOUR
                 )
         );
     }
